c19096ad5ac849cbd4540fedbad8d942a17aee06
poojith <you@example.com>
2026-03-03 09:02:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Country from "./Country"
+function App() {
+
+  return (
+    <div>
+      <Country />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Country.jsx
@@ -0,0 +1,80 @@
+import { useEffect,useState } from "react";
+import "./Country.css";
+function Country(){
+const [countries,setCountries] = useState([]);
+const [states,setStates] = useState([]);
+const [city,setCity] = useState([]);
+
+const [selectedCountries,setSelectedCountries] = useState("");
+const [selectedStates,setSelectedStates] = useState("");
+const [selectedCity,setSelectedCity] = useState("");
+//fecting countries data
+useEffect(()=>{
+    fetch("https://location-selector.labs.crio.do/countries")
+    .then((res)=>res.json())
+    .then((data)=>setCountries(data))
+    .catch((err) => console.log(err));
+
+},[])
+useEffect(()=>{
+if(selectedCountries){
+    fetch(`https://location-selector.labs.crio.do/country=${selectedCountries}/states`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        setStates(data);
+        setSelectedStates("");
+        setCity([]);
+        setSelectedCity("");
+    })
+    .catch((err)=>console.log(err))
+}
+},[selectedCountries])
+useEffect(()=>{
+    if(selectedCountries || selectedStates){
+        fetch(`https://location-selector.labs.crio.do/country=${selectedCountries}/state=${selectedStates}/cities`)
+        .then((res)=>res.json())
+        .then((data)=>{
+            setCity(data);
+            setSelectedCity("");
+        })
+        .catch((err)=>console.log(err))
+    }
+},[selectedStates])
+
+
+return(
+    <div className="container">
+        <h1 className="heading">Select Location</h1>
+        <div className="dropdown">
+            <select value={selectedCountries}
+        onChange={(e)=>setSelectedCountries(e.target.value)}>
+            <option value="">Select Country</option>
+            {countries.map((c)=>(
+                <option  key={c} value={c}>{c}</option>
+            ))}
+        </select>
+        <select value={selectedStates}
+        onChange={(e)=>setSelectedStates(e.target.value)}>
+            <option value="">Select State</option>
+            {states.map((s)=>(
+                <option  key={s} value={s}>{s}</option>
+            ))}
+        </select>
+        <select value={selectedCity}
+        onChange={(e)=>setSelectedCity(e.target.value)}>
+            <option value="">Select City</option>
+            {city.map((city)=>(
+                <option  key={city} value={city}>{city}</option>
+            ))}
+        </select>
+        </div>
+        {selectedCity && (
+        <h3>
+          You selected {selectedCity}, {selectedStates}, {selectedCountries}
+        </h3>
+      )}
+    </div>
+)
+}
+
+export default Country;
